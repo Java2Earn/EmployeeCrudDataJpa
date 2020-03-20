e5416c5b6af4c48d01bf350fe03e51d572b66508
akashTrainer <52851863+akashTrainer@users.noreply.github.com>
2020-03-20 14:38:04 +0530

    first commit

---
+++ BasicEx/src/com/java2earn/Test.java
@@ -0,0 +1,24 @@
+package com.java2earn;
+
+public class Test {
+	
+	void m1(int a)
+	{
+		
+	}
+	 void m2(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+public static void main(String[] args) {
+	Test t=new Test();//default values
+		t.m1(12);//stack thread1
+		t.m2(22);
+		t.m1(12);//stack thread1
+		t.m2(22);
+		t.notify();//native
+		
+}
+
+
+}
